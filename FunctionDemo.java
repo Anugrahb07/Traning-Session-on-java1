1a29b35165b3dcbc6fb77b31d77766dbbe8461bd
Anugrahb07 <anugrahbandgar@gmail.com>
2024-06-11 15:02:04 +0530

    Function in java

---
+++ FunctionDemo.java
@@ -0,0 +1,17 @@
+package sam.com;
+
+public class FunctionDemo {
+	public static void add() {
+		int a=10;
+		int b=20;
+		int total=a+b;
+		System.out.println("Addition=" +total);
+	}
+	
+		public static void sub(int c,int d) {
+			
+			int total=c-d;
+			System.out.println("Sub=" +total);
+	}
+
+}
---
+++ TestFunctions.java
@@ -0,0 +1,11 @@
+package sam.com;
+
+public class TestFunctions {
+	public static void main(String[] args) {
+		//FunctionDemo fd=new FunctionDemo();
+		FunctionDemo.add();
+		FunctionDemo.sub(12,15);//calling function
+	}
+	
+
+}
